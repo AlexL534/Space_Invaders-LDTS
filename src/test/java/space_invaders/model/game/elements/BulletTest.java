9624d26eca0e413601136ff2bf96d13ba15b1c22
AlexL534 <alexandrefernadeslopes@gmail.com>
2024-10-08 12:19:51 +0100

    fix

--- src/test/java/space_invaders/model/game/elements/BulletTest.java
+++
@@ -1,9 +0,0 @@
-package space_invaders.model.game.elements;
-
-import com.aor.space_invaders.Position;
-import com.aor.space_invaders.model.game.elements.powerups.PowerUp;
-
-public class BulletTest {
-
-}
-
